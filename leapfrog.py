b432497590d22a2771d189cc5c1b3eb8c4785ada
Migelo <miha@filetki.si>
2021-01-12 10:41:44 +0100

    fix normal integration

--- leapfrog.py
+++ leapfrog.py
@@ -15,7 +15,7 @@ class objec:
         self.vy = vy
         self.vz = vz
         self.name = name
-        self.pos = []
+        self.pos = [pos]
 
     def initial_force(self, objects):
         self.ax, self.ay, self.az = self.acceleration(objects)
@@ -35,31 +35,32 @@ class objec:
                 d = np.sqrt(dx ** 2 + dy ** 2 + dz ** 2)
                 dxy = np.sqrt(dx ** 2 + dy ** 2)
 
-                F = -self.mass * body.mass / (d ** 2)
+                F = - body.mass / (d ** 2)
 
-                theta = np.arccos(dz / d)
+                costheta = dz / d
+                sintheta = dxy / d
                 cosphi = dx / dxy
                 sinphi = dy / dxy
 
-                f1 = F * np.sin(theta) * cosphi
-                f2 = F * np.sin(theta) * sinphi
-                f3 = F * np.cos(theta)
+                f1 = F * sintheta * cosphi
+                f2 = F * sintheta * sinphi
+                f3 = F * costheta
 
                 fx += f1
                 fy += f2
                 fz += f3
-        return fx / self.mass, fy / self.mass, fz / self.mass
+        return fx, fy, fz
 
     def movement(self, objects, day):
         ax, ay, az = self.acceleration(objects)
 
-        self.x += self.vx * day
-        self.y += self.vy * day
-        self.z += self.vz * day
+        self.x += self.vx * day + ax * day**2
+        self.y += self.vy * day + ay * day**2
+        self.z += self.vz * day + az * day**2
 
-        self.vx += ((ax + self.ax) / 2) * day
-        self.vy += ((ay + self.ay) / 2) * day
-        self.vz += ((az + self.az) / 2) * day
+        self.vx += ((ax + self.ax*0)) * day
+        self.vy += ((ay + self.ay*0)) * day
+        self.vz += ((az + self.az*0)) * day
 
         self.ax = ax
         self.ay = ay
@@ -78,15 +79,14 @@ class objec:
 def main():
     # defining each object
     Sun = objec((0, 0, 0), 1, "Sun", "#d40404")
-    Earth = objec((1, 0, 0), 3.00348959632e-6, "Earth", "#0985eb", 0, 0, 1)
+    Earth = objec((1, 0, 0), 3.00348959632e-6, "Earth", "#0985eb", 0, 1, 0)
 
     objects = [Sun, Earth]
     for obje in objects:
         obje.initial_force(objects)
 
 
-    #time = np.arange(0, 100*2*np.pi, 2*np.pi/365)
-    time = np.arange(0, 10*2*np.pi, 2*np.pi/365)
+    time = np.arange(0, 2*np.pi, 2*np.pi/365)
 
     day = time[1]
 
